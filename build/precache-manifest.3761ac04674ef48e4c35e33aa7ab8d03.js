f309514496dd23e842932d4b92981bd49099edd0
Dipayan Mukherjee <56663132+cruse1914@users.noreply.github.com>
2021-03-16 15:23:38 +0530

    Added all files

---
+++ build/precache-manifest.3761ac04674ef48e4c35e33aa7ab8d03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5710005683ff90281011ed648a74dff7",
+    "url": "/React-contacts/index.html"
+  },
+  {
+    "revision": "f2d3e5a57f2d99a2d912",
+    "url": "/React-contacts/static/js/2.ead49188.chunk.js"
+  },
+  {
+    "revision": "f207054080433a8b484a",
+    "url": "/React-contacts/static/js/main.d04aa751.chunk.js"
+  },
+  {
+    "revision": "6d018c06e2d4becd27e5",
+    "url": "/React-contacts/static/js/runtime-main.fcfc054e.js"
+  }
+]);
